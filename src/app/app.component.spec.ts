db4f3c837db0d0eae45335c3203148f3aabce823
Sergio Barreras <sergiobg.isc@gmail.com>
2024-08-28 11:22:05 -0700

    Evaluar clases y atributos HTML

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -27,4 +27,30 @@ describe('AppComponent', () => {
   it('should render routed-outlet', () => {
     expect(compiled.querySelector('router-outlet')).not.toBeNull();
   });
+
+  it('should render router-outlet wrapped with css classes', () => {
+    const divElement = compiled.querySelector('div');
+    const mustHaveClasses =
+      'min-w-screen min-h-screen bg-slate-600 flex items-center justify-center px-5 py-5'.split(
+        ' '
+      );
+
+    expect(divElement).not.toBeNull();
+
+    const divClasses = divElement?.classList.value.split(' ');
+
+    mustHaveClasses.forEach((className) => {
+      expect(divClasses).toContain(className);
+    });
+  });
+
+  it('should contain the buy me a beer link', () => {
+    const anchorElement = compiled.querySelector('a');
+
+    expect(anchorElement).not.toBeNull();
+    expect(anchorElement?.title).toBe('Buy me a beer');
+    expect(anchorElement?.href).toBe(
+      'https://www.buymeacoffee.com/scottwindon'
+    );
+  });
 });
