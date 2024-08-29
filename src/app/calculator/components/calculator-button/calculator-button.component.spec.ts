c1730f335492e62e987126d917d82f5101db3c55
Sergio Barreras <sergiobg.isc@gmail.com>
2024-08-28 18:57:20 -0700

    Probando contenido proyectado

--- src/app/calculator/components/calculator-button/calculator-button.component.spec.ts
+++ src/app/calculator/components/calculator-button/calculator-button.component.spec.ts
@@ -1,5 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CalculatorButtonComponent } from './calculator-button.component';
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [CalculatorButtonComponent],
+  template: `
+    <calculator-button>
+      <span class="projected-content underline">Test content</span>
+    </calculator-button>
+  `,
+})
+class TestHostComponent {}
 
 describe('CalculatorButtonComponent', () => {
   let fixture: ComponentFixture<CalculatorButtonComponent>;
@@ -46,7 +58,7 @@ describe('CalculatorButtonComponent', () => {
     expect(component.onClick.emit).toHaveBeenCalled();
   });
 
-  it('should set isPressed to true and then false when keyboardPressedStyle is called with matching key ', (done) => {
+  it('should set isPressed to true and then false when keyboardPressedStyle is called with matching key', (done) => {
     component.contentValue()!.nativeElement.innerText = '1';
     component.keyboardPressedStyle('1');
 
@@ -56,5 +68,22 @@ describe('CalculatorButtonComponent', () => {
       expect(component.isPressed()).toBeFalse();
       done();
     }, 101);
+  });
+
+  it('should not set isPressed to true if key is no matching', () => {
+    component.contentValue()!.nativeElement.innerText = '1';
+    component.keyboardPressedStyle('2');
+
+    expect(component.isPressed()).toBeFalse();
+  });
+
+  it('should display projected content', () => {
+    const testHostFixture = TestBed.createComponent(TestHostComponent);
+    const divElement = testHostFixture.nativeElement as HTMLDivElement;
+
+    const projectedContent = divElement.querySelector('.projected-content');
+
+    expect(projectedContent).not.toBeNull();
+    expect(projectedContent?.classList.contains('underline')).toBeTrue();
   });
 });
